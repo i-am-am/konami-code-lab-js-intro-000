c598d95bbc10f4354255fa6173076990d5d232d0
Alena <i.am.am.629@gmail.com>
2018-08-03 21:33:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,10 +12,10 @@ const codes = [
 ];
 
 function init() {
+  let index = 0;
   const input = document.querySelector('input')
   document.body.addEventListener('keydown', function(e) {
     const key = e.which
-    let index = 0;
     if (key === codes[index]) {
       index++;
       if (index === codes.length) {
